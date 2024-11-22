7aedc7d0e37602f758fe91c4c0db1c7bb627a872
Bukharkin Andrey <osetr013rus@gmail.com>
2024-11-22 19:31:35 +0300

    Инициализировал проект

--- HW1/src/main/java/org/example/Main.java
+++ HW1/src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
-
-
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String words = scanner.nextLine();
+        List<String> addString = new ArrayList<>();
 
+        while(!words.isEmpty()){
+            addString.add(words);
+            words = scanner.nextLine();
+        }
+
+        System.out.println(guavaCountWordsClassExample(addString, scanner.nextLine()));
+    }
+
+    static int guavaCountWordsClassExample(List<String> words, String countOfWords){
+        Multiset<String> multiset = HashMultiset.create();
+        multiset.addAll(words);
+        return multiset.count(countOfWords);
     }
 }
